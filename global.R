1015277adf5ecd908d71fcd77ec42df2f272aa22
KleberNeves <kleber.na@gmail.com>
2020-11-11 19:30:33 -0300

    Tidy up the sims dataset to have the specification columns

--- global.R
+++ global.R
@@ -1,32 +1,43 @@
 library(shiny)
 library(tidyverse)
 
-N = 1000
+load(file = "sim_data.RData")
+SIMS = repdata
+rm(repdata)
 
-SIMS = data.frame(
-  index = 1:N,
-  param1 = sample(c(T,F), N, replace = T),
-  param2 = runif(N, 0, 1),
-  param3 = sample(1:4, N, replace = T)
-) %>%
+SIMS = SIMS %>%
+  rename(AboveMin = `% Above minimum`)
+
+SIMS$weightB = ifelse(SIMS$AboveMin %in% c(0.2,0.4,0.6,0.8), SIMS$AboveMin,
+  ifelse(SIMS$AboveMin == 0.21, 0.2,
+         ifelse(SIMS$AboveMin == 0.41, 0.4,
+                ifelse(SIMS$AboveMin == 0.61, 0.6,
+                       ifelse(SIMS$AboveMin == 0.62, 0.6, 0.8)))))
+SIMS = SIMS %>%
   mutate(
-    repro_rate = (30 + param1 * 40 - param2 * 30 + param3 * 7.5) / 100
+    weight.label = paste0("% Above Min. = ", 100 * weightB, "%"),
+    power.label = paste0("Power = ", 100 * typical.power, "%"),
+    bias.label = paste0("Bias = ", 100 * bias.level, "%"),
+    interlab.label = paste0("Interlab Var. = ", round(100 * interlab.var / (interlab.var + 1), 0), "%")
   )
 
-# Transforming all parameters in booleans
-SIMS = SIMS %>% mutate(
-  # Logical parameters: remain the same
-  param1_T = param1,
-  # Numerical parameters: are broken into a few categories
-  param2_0.1 = param2 >= 0 & param2 < 0.25,
-  param2_0.3 = param2 >= 0.25 & param2 < 0.5,
-  param2_0.5 = param2 >= 0.5 & param2 < 0.75,
-  param2_0.7 = param2 >= 0.75 & param2 <= 1,
-  
-  param3_1 = param3 == 1,
-  param3_2 = param3 == 2,
-  param3_3 = param3 == 3,
-  param3_4 = param3 == 4
+# Transforming all parameters in indicators
+parameters_cols = c("weight.label","bias.label","interlab.label","power.label")
+
+SIMS_WIDE = bind_cols(
+  map(1:length(parameters_cols), function (i) {
+    par_col = parameters_cols[i]
+    values = unique(SIMS[[par_col]])
+    DF = bind_cols(
+      map(values, function (x) {
+        df = data.frame(value = SIMS[[par_col]] == x)
+        colnames(df) = x
+        df
+      })
+    )
+  })
 )
 
+SIMS = bind_cols(list(SIMS, SIMS_WIDE))
+  
 SELECTION = SIMS
